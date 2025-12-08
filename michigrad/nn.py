72306e589ef4e8f981eda3df74f0d322f416ce51
lucasquieresopa <101714093+lucasquieresopa@users.noreply.github.com>
2025-12-08 16:14:07 -0300

    comentarios a lo loco en nn.py

--- michigrad/nn.py
+++ michigrad/nn.py
@@ -20,7 +20,11 @@ class Neuron(Module):
     def __call__(self, x):
         # realiza la forward pass de la neurona 
         # x es un arreglo
-        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+        # pasar nonlin = False para usar función de activación lineal
+        # nonlin = True usa ReLU
+        # puede modificarse para implementar cualquier función de activ.
+        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b) 
+            # zip combina wi con xi
         return act.relu() if self.nonlin else act
 
     def parameters(self):
@@ -30,27 +34,50 @@ class Neuron(Module):
         return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
 class Layer(Module):
+    # Define una capa de neuronas
 
     def __init__(self, nin, nout, **kwargs):
-        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+        # recibe cantidad de entradas (nin) y salidas (nout)
+        # p.e : Layer(2, 1) es una neurona que recibe dos entradas y tiene una salida
+        # puede recibir nonlin
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)] 
+            # lista de neuronas
+            # todas con la misma cantidad de entradas y salidas
 
     def __call__(self, x):
-        out = [n(x) for n in self.neurons]
+        # aplica la función de activación a cada neurona de la capa
+        # x es el valor de la entrada
+        out = [n(x) for n in self.neurons] 
+            # se aplica la función de activación a cada neurona
+            # se llama a Neuron.__call__(x)
+            
         return out[0] if len(out) == 1 else out
+            # out es un arreglo de tamaño nout
 
     def parameters(self):
-        return [p for n in self.neurons for p in n.parameters()]
+        return [p for n in self.neurons for p in n.parameters()] # la falsedad de la expresividad de python
 
     def __repr__(self):
         return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
 class MLP(Module):
-
+    # perceptron multicapa
+    # p.e : MLP(2, [3, 3, 1]) es un ppn con 
+    #   capa de entrada dos entradas
+    #   una hidden con 3 entradas
+    #   segunda hidden con 3 entradas
+    #   capa de salida con una entrada
     def __init__(self, nin, nouts):
-        sz = [nin] + nouts
+        sz = [nin] + nouts # lista que se recorre para crear las capas
         self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+            # cada capa se crea con sus entradas y las entradas de la capa siguiente
+            # capa de entrada: 2, 3
+            # hidden 1 : 3, 3
+            # hidden 2 : 3, 1
+            # salida : 1 (no tiene salida)
 
     def __call__(self, x):
+        # llama a la función que define cada Layer
         for layer in self.layers:
             x = layer(x)
         return x
